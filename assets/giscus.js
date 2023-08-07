9b5307e80b203fe81d4ae32479e89d03adcf1871
PJ568 <PJ568@PJ568.eu.org>
2023-08-07 16:31:58 +0800

    添加文章评论区

---
+++ assets/giscus.js
@@ -0,0 +1,32 @@
+// 若当前 mdbook 主题为 Light 或 Rust ，则将 giscuz 主题设置为 light
+var theme = "transparent_dark";
+const themeClass = document.getElementsByTagName("html")[0].className;
+if (themeClass.indexOf("light") != -1 || themeClass.indexOf("rust") != -1) {
+    theme = "light"
+}
+
+var giscus = function () {
+	const script = document.createElement("script");
+	script.type = "text/javascript";
+	script.src = "https://giscus.app/client.js";
+
+
+	script.setAttribute("data-repo", "Hamud-Lang/Hamud_Book");
+	script.setAttribute("data-repo-id", "R_kgDOKAX-uw");
+	script.setAttribute("data-category", "文章评论区");
+	script.setAttribute("data-category-id", "DIC_kwDOKAX-u84CYRSl");
+
+	script.setAttribute("data-mapping", "title");
+	script.setAttribute("data-strict", "1");
+	script.setAttribute("data-reactions-enabled", "1");
+	script.setAttribute("data-emit-metadata", "0");
+	script.setAttribute("data-input-position", "top");
+	script.setAttribute("data-theme", theme);
+	script.setAttribute("data-lang", "zh-CN");
+
+	script.crossOrigin = "anonymous";
+	script.async = true;
+	document.getElementById("giscus-container").appendChild(script);
+};
+
+window.addEventListener('load', giscus);

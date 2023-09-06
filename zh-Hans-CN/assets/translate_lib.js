04eeeadfdb8c248d77b0b853a4f9827cd4f1cc60
PJ-568 <LiuTian-PJ568@PJ568.eu.org>
2023-09-06 16:38:01 +0000

    Deploying to gh-pages from @ Hamud-Lang/Hamud_Book@72199029202cee1875c0c3d78570bed853f967bd 🚀

--- zh-Hans-CN/assets/translate_lib.js
+++ zh-Hans-CN/assets/translate_lib.js
@@ -10,10 +10,13 @@ translate.ignore.class.push('notTranslate');
 translate.nomenclature.append('chinese_simplified','english',`
     哈姆语言之书=Hamud Book
     哈姆语=the language of Hamud
+    哈姆德=the Hamud
+    哈姆人=hamud
     哈姆=Hamud
+    哈约·姆经=Hamud the Bible
     哈经·姆约=Hamud the Bible
     哈经=Hamud
     姆约=the Bible
-    哈约=the Bible
+    哈约=the Byble
 `);
 translate.execute();

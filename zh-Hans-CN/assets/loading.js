62f88b8ceefcdcfd4e2e9f353a0d770d7ce4e872
PJ-568 <LiuTian-PJ568@PJ568.eu.org>
2023-09-16 06:09:58 +0000

    Deploying to gh-pages from @ Hamud-Lang/Hamud_Book@ecb5a1553c43353979281aa3a62b0275a9a7de86 🚀

--- zh-Hans-CN/assets/loading.js
+++ zh-Hans-CN/assets/loading.js
@@ -3,20 +3,30 @@ document.onreadystatechange=function () {
          loadingFade();
     }
 }
-function loadingFade() {
-var opacity=1;
-const position = document.querySelector('#loading');
-//  var loadingPage=document.getElementById('loading');
-var loadingBackground=document.getElementById('loading_bg');
-var time=setInterval(function () {
-    if (opacity<=0){
-        clearInterval(time);
-        //  loadingPage.remove();
-        //  $('#loading').remove();
-        position.style.display = 'none';
-    }
 
-    loadingBackground.style.opacity=opacity;
-    opacity-=0.4;
-},100);
+// const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
+const position = document.querySelector('#loading');
+
+function startLoading() {
+    var loadingBackground=document.getElementById('loading_bg');
+    position.style.display = 'block';
+    loadingBackground.style.opacity=1;
+    setTimeout(() => loadingFade(), 30000)
+}
+
+function loadingFade() {
+    var opacity=1;
+    //  var loadingPage=document.getElementById('loading');
+    var loadingBackground=document.getElementById('loading_bg');
+    var time=setInterval(function () {
+        if (opacity<=0){
+            clearInterval(time);
+            //  loadingPage.remove();
+            //  $('#loading').remove();
+            position.style.display = 'none';
+        }
+
+        loadingBackground.style.opacity=opacity;
+        opacity-=0.4;
+    },100);
 }

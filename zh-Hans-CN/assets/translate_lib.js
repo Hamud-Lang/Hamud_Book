8535081bed937ed30545777ed98abe16b0f7cf7b
PJ-568 <PJ568@PJ568.eu.org>
2023-10-20 03:10:34 +0000

    Deploying to gh-pages from @ Hamud-Lang/Hamud_Book@b072b8d6e9ac7673f1f6c31a2984473384e8c27c 🚀

--- zh-Hans-CN/assets/translate_lib.js
+++ zh-Hans-CN/assets/translate_lib.js
@@ -4,19 +4,19 @@ try{
     translate.setAutoDiscriminateLocalLanguage();
     translate.language.setUrlParamControl();
     translate.ignore.class.push('notTranslate');
+    translate.nomenclature.append('chinese_simplified','english',`
+        哈姆语言之书=Hamud Book
+        哈姆语=the language of Hamud
+        哈姆德=the Hamud
+        哈姆人=hamud
+        哈姆=Hamud
+        哈约·姆经=Hamud the Bible
+        哈经·姆约=Hamud the Bible
+        哈经=Hamud
+        姆约=the Bible
+        哈约=the Byble
+    `);
 }
 catch(e){console.log(e);}
 translate.setUseVersion2();
-translate.nomenclature.append('chinese_simplified','english',`
-    哈姆语言之书=Hamud Book
-    哈姆语=the language of Hamud
-    哈姆德=the Hamud
-    哈姆人=hamud
-    哈姆=Hamud
-    哈约·姆经=Hamud the Bible
-    哈经·姆约=Hamud the Bible
-    哈经=Hamud
-    姆约=the Bible
-    哈约=the Byble
-`);
 translate.execute();

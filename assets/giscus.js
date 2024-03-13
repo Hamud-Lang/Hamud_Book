dabbb1b9ec7687d59fce26978704b6e0fa088695
PJ568 <PJ568@PJ568.eu.org>
2024-03-13 17:53:28 +0800

    [Optimize] 取消评论区懒加载

--- assets/giscus.js
+++ assets/giscus.js
@@ -59,7 +59,7 @@ var SetupGiscus = function (giscus_lang, theme) {
 	script.setAttribute("data-input-position", "top");
 	script.setAttribute("data-theme", theme);
 	script.setAttribute("data-lang", giscus_lang);
-	script.setAttribute("data-loading", "lazy");
+	// script.setAttribute("data-loading", "lazy");
 
 	script.crossOrigin = "anonymous";
 	script.async = true;

50bb37c371a37f52c964c034f9fe0d86772c697f
PJ-568 <PJ568@PJ568.eu.org>
2024-03-13 09:25:51 +0000

    Deploying to gh-pages from @ Hamud-Lang/Hamud_Book@e78be32549793cb83041ca76f2514f57b091046e 🚀

---
+++ zh-Hans-CN/assets/SetPjax.js
@@ -0,0 +1,20 @@
+var pjax = new Pjax({
+    selectors: [
+        "head title",
+        "head meta",
+        ".theme-popup",
+        ".chapter",
+        ".content",
+        ".pjax-reload"
+    ],
+});
+
+// 包含一些 Pjax 加载后需要重新执行的函数
+function pjax_reload() {
+    SetupGiscus(getCurrentLanguage(), getCurrentTheme());
+}
+
+// 监听 Pjax 完成后，重新加载上面的函数
+document.addEventListener("pjax:complete", function () {
+    pjax_reload();
+});
--- zh-Hans-CN/assets/giscus.js
+++ zh-Hans-CN/assets/giscus.js
@@ -1,41 +1,47 @@
 // 若当前 mdbook 主题为 Light 或 Rust ，则将 giscus 主题设置为 light
-var theme = "transparent_dark";
-const themeClass = document.getElementsByTagName("html")[0].className;
-if (themeClass.indexOf("light") != -1 || themeClass.indexOf("rust") != -1) {
-    theme = "light"
+const getCurrentTheme = function () {
+	var theme = "transparent_dark";
+	const themeClass = document.getElementsByTagName("html")[0].className;
+	if (themeClass.indexOf("light") != -1 || themeClass.indexOf("rust") != -1) {
+		theme = "light"
+	}
+	return theme;
 }
 
 // 获取用户使用的语种。
-var lang = translate.language.getCurrent();
-var giscus_lang = "zh-CN";
-switch (lang) {
-	case "chinese_traditional":
-		giscus_lang = "zh-TW";
-		break;
-	case "english":
-		giscus_lang = "en";
-		break;
-	case "spanish":
-		giscus_lang = "es";
-		break;
-	case "japanese":
-		giscus_lang = "ja";
-		break;
-	case "korean":
-		giscus_lang = "ko";
-		break;
-	case "french":
-		giscus_lang = "fr";
-		break;
-	case "arabic":
-		giscus_lang = "ar";
-		break;
-	default:
-		giscus_lang = "zh-CN";
-		break;
-}
+const getCurrentLanguage = function () {
+	var lang = translate.language.getCurrent();
+	var giscus_lang = "zh-CN";
+	switch (lang) {
+		case "chinese_traditional":
+			giscus_lang = "zh-TW";
+			break;
+		case "english":
+			giscus_lang = "en";
+			break;
+		case "spanish":
+			giscus_lang = "es";
+			break;
+		case "japanese":
+			giscus_lang = "ja";
+			break;
+		case "korean":
+			giscus_lang = "ko";
+			break;
+		case "french":
+			giscus_lang = "fr";
+			break;
+		case "arabic":
+			giscus_lang = "ar";
+			break;
+		default:
+			giscus_lang = "zh-CN";
+			break;
+	}
+	return giscus_lang;
+};
 
-var giscus = function () {
+var SetupGiscus = function (giscus_lang, theme) {
 	const script = document.createElement("script");
 	script.type = "text/javascript";
 	script.src = "https://giscus.app/client.js";
@@ -53,10 +59,13 @@ var giscus = function () {
 	script.setAttribute("data-input-position", "top");
 	script.setAttribute("data-theme", theme);
 	script.setAttribute("data-lang", giscus_lang);
+	script.setAttribute("data-loading", "lazy");
 
 	script.crossOrigin = "anonymous";
 	script.async = true;
-	document.getElementById("giscus-container").appendChild(script);
+	if (document.getElementById("giscus-container") != null) {
+		document.getElementById("giscus-container").appendChild(script);
+	}
 };
 
-window.addEventListener('load', giscus);
+window.addEventListener('load', () => SetupGiscus(getCurrentLanguage(), getCurrentTheme()));

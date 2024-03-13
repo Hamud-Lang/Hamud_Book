13752c4dd990aa512f01e757a6b54c76c9f5a410
PJ568 <PJ568@PJ568.eu.org>
2024-03-13 16:07:36 +0800

    [New] 新增 Pjax 支持

--- js/giscus.js
+++ js/giscus.js
@@ -1,34 +1,36 @@
-// 获取用户使用的语种。
-var lang = translate.language.getCurrent();
-var giscus_lang = "zh-CN";
-switch (lang) {
-	case "chinese_traditional":
-		giscus_lang = "zh-TW";
-		break;
-	case "english":
-		giscus_lang = "en";
-		break;
-	case "spanish":
-		giscus_lang = "es";
-		break;
-	case "japanese":
-		giscus_lang = "ja";
-		break;
-	case "korean":
-		giscus_lang = "ko";
-		break;
-	case "french":
-		giscus_lang = "fr";
-		break;
-	case "arabic":
-		giscus_lang = "ar";
-		break;
-	default:
-		giscus_lang = "zh-CN";
-		break;
-}
+const getCurrentLanguage = function () {
+	var lang = translate.language.getCurrent();
+	var giscus_lang = "zh-CN";
+	switch (lang) {
+		case "chinese_traditional":
+			giscus_lang = "zh-TW";
+			break;
+		case "english":
+			giscus_lang = "en";
+			break;
+		case "spanish":
+			giscus_lang = "es";
+			break;
+		case "japanese":
+			giscus_lang = "ja";
+			break;
+		case "korean":
+			giscus_lang = "ko";
+			break;
+		case "french":
+			giscus_lang = "fr";
+			break;
+		case "arabic":
+			giscus_lang = "ar";
+			break;
+		default:
+			giscus_lang = "zh-CN";
+			break;
+	}
+	return giscus_lang;
+};
 
-var giscus = function () {
+var SetupGiscus = function (giscus_lang) {
 	const script = document.createElement("script");
 	script.type = "text/javascript";
 	script.src = "https://giscus.app/client.js";
@@ -46,10 +48,13 @@ var giscus = function () {
 	script.setAttribute("data-input-position", "top");
 	script.setAttribute("data-theme", "light_high_contrast");
 	script.setAttribute("data-lang", giscus_lang);
+	script.setAttribute("data-loading", "lazy");
 
 	script.crossOrigin = "anonymous";
 	script.async = true;
-	document.getElementById("giscus-container").appendChild(script);
+	if (document.getElementById("giscus-container") != null) {
+		document.getElementById("giscus-container").appendChild(script);
+	}
 };
 
-window.addEventListener('load', giscus);
+window.addEventListener('load', () => SetupGiscus(getCurrentLanguage()));
